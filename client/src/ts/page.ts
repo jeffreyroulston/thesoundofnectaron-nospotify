427954a434e578379305fce9a05848c2852283b5
Celine <celinegiovanni92@gmail.com>
2020-08-09 20:02:40 +1200

    restructure lol

--- client/src/ts/page.ts
+++ client/src/ts/page.ts
@@ -1,54 +1,60 @@
-import {QueryParameters} from "./spotify-interface";
-import {SliderQuestion, MCQuestion, QuickFireQuestion, createSliderQuestion} from "./questions";
+// import {QueryParameters} from "./spotify-interface";
+// import {Question, SliderQuestion, MCQuestion, QuickFireQuestion} from "./questions";
 
-var colours = {
-    beige : "#FCF1DB",
-    red : "#FF2000"
-}
+// var colours = {
+//     beige : "#FCF1DB",
+//     red : "#FF2000"
+// }
 
-export enum PageType {
-    Login,
-    RoundName,
-    Question
-}
+// export enum PageType {
+//     Login,
+//     RoundName,
+//     Question
+// }
 
-export interface Page {
-    assets : string[];
-    pageType : PageType,
-    question : SliderQuestion | MCQuestion | QuickFireQuestion | undefined,
-    pageElement : string,
-    bgColour : string,
-}
+// export class Page {
+//     pageType : PageType
+
+//     constructor() {}
+// }
 
 
-export var allPages : Page[] = [
-    {
-        assets : [],
-        pageType : PageType.Login,
-        question : undefined,
-        pageElement : "#login",
-        bgColour : colours.beige,
+// export interface Page {
+//     assets : string[];
+//     pageType : PageType,
+//     question : Question | undefined,
+//     pageElement : string,
+//     bgColour : string,
+// }
+
+// export var allPages : Page[] = [
+//     {
+//         assets : [],
+//         pageType : PageType.Login,
+//         question : undefined,
+//         pageElement : "#login",
+//         bgColour : colours.beige,
         
-    },
-    {
-        assets : [],
-        pageType : PageType.RoundName,
-        question : undefined,
-        pageElement : "#roundName",
-        bgColour : colours.red,
+//     },
+//     {
+//         assets : [],
+//         pageType : PageType.RoundName,
+//         question : undefined,
+//         pageElement : "#roundName",
+//         bgColour : colours.red,
         
-    },
-    {
-        assets : [],
-        pageType : PageType.Question,
-        question : createSliderQuestion(
-            QueryParameters.Valence,
-            "How bitter would you like your brew?",
-            0,
-            100
-        ),
-        pageElement : "#slider-q",
-        bgColour : colours.beige,
+//     },
+//     {
+//         assets : [],
+//         pageType : PageType.Question,
+//         question : createSliderQuestion(
+//             QueryParameters.Valence,
+//             "How bitter would you like your brew?",
+//             0,
+//             100
+//         ),
+//         pageElement : "#slider-q",
+//         bgColour : colours.beige,
         
-    }
-]
+//     }
+// ]
--- client/src/ts/ui.ts
+++ client/src/ts/ui.ts
@@ -1,29 +1,102 @@
 import * as si from "./spotify-interface";
 import {Question, QuestionType, QueryParameter, SliderQuestion} from "./questions";
-// import Page from "./page";
-import {Page, PageType, allPages} from "./page";
-import {TweenLite} from "gsap"
+import {TweenLite, TweenMax} from "gsap"
 
 var qDefault = function() { return { value: 0, include: false } };
 
+enum PageType {
+    Login,
+    RoundName,
+    Question
+}
+
+class Slider {
+    private el : string;
+    private sliderEl : HTMLInputElement;
+    private sliderThumb : HTMLElement;
+    private topFruitElement : HTMLElement;
+    private bottomFruitElement : HTMLElement;
+
+    private min : number = 0;
+    private max : number = 0;
+    private value : number = 0;
+    private prevValue : number = 0
+    private sliderWidth : number = 0;
+    private fruitDefaultWidth : number = 200;
+
+    constructor(elementName: string) {
+        this.el = elementName;
+        this.sliderEl = <HTMLInputElement>el(this.el + " .slider-input");
+        this.sliderThumb = el(this.el + " .slider-thumb");
+        this.topFruitElement = el(this.el+ " .fruit-top img");
+        this.bottomFruitElement = el(this.el + " .fruit-bottom img");
+
+        // set bindings
+        this.sliderEl.addEventListener("input",this.sliderChange.bind(this));
+    }
+
+    set(q : SliderQuestion) {
+        this.min = q.minValue;
+        this.max = q.maxValue;
+
+        // set slider value to the middle
+        var initial = (this.max - this.min)/2;
+        this.value = initial;
+        this.prevValue = initial;
+
+        // apply to slider element
+        this.sliderEl.min = this.min.toString();
+        this.sliderEl.max = this.max.toString();
+        this.sliderEl.value = initial.toString();
+    }
+
+    sliderChange(e: any){
+        // get the width and the value of the slider 
+        this.sliderWidth = e.srcElement.clientWidth;
+        this.value= e.srcElement.value;
+
+        // get the next position of the arrow
+        // move the triangle to match the position of the slider thumb
+        this.sliderThumb.style.left = (((this.value - this.min) / (this.max - this.min) * (this.sliderWidth)) - this.sliderThumb.getBoundingClientRect().width/2).toString() + "px"
+    }
+}
 
 export default class UI {
     // PRIVATE VARIABLES
-    private pages = allPages;
-    private currentPage = this.pages[0];
-    private previousPage : Page | undefined = undefined;
-    private currentPageIdx = 0;
+    // private pages = allPages;
+    // private currentPage = this.pages[0];
+    // private previousPage : Page | undefined = undefined;
+    // private currentPageIdx = 0;
 
     // this is horrible, fix it
-    private currentQuestion : any = undefined;
+    // private currentQuestion : any = undefined;
 
-    // SLIDER VARIABLES
-    private sliderArrowEl = el("#slider-thumb");
-    private sliderTopFruitEl = el(".fruit-top img");
-    private sliderBottomFruitEl = el(".fruit-bottom img");
-    private sliderWidth = 0;
-    private sliderValue = 0;
-    private sliderPreviousValue = 0;
+    private currentPage : PageType = PageType.Login;
+
+    private slider = new Slider("#slider-q")
+    private questions : SliderQuestion[] = [
+        {
+            type: QuestionType.Slider,
+            params: si.QueryParameters.Valence,
+            question : "How bitter would you like your brew?",
+            minValue : 0,
+            maxValue : 100,
+            answer : 0
+        },
+        {
+            type: QuestionType.Slider,
+            params: si.QueryParameters.Valence,
+            question : "How tangy would you like your brew?",
+            minValue : 0,
+            maxValue : 100,
+            answer : 0
+        }
+    ]
+
+    private colours = {
+        beige : "#FCF1DB",
+        red : "#FF2000"
+    }
 
     private recommendations: si.Track[] | undefined = [];
     private queryParameters: {[key: string]: QueryParameter }  = {
@@ -47,115 +120,135 @@ export default class UI {
     }
     
     private init() {
-        this.setCurrentPage();
-
         // set button bindings
         el("#startBtn").addEventListener("click", this.Login.bind(this));
-        el(".next").addEventListener("click", this.getNextPage.bind(this));
-        el("#sliderInput").addEventListener("input",this.sliderChange.bind(this));
-        // el("#sliderInput").addEventListener("change", this.sliderInput)
+        el(".next").addEventListener("click", this.next.bind(this));
+        
+        this.showLogin();
     }
 
-    private setCurrentPage() {
-        console.log(this.currentPage);
-
-        // hide current page
-        if (this.previousPage) {
-            // el(this.previousPage.pageElement).classList.toggle("active");
-            el(this.previousPage.pageElement).style.display = "none";
-        }
-
-        // show the next page
-        // el(this.currentPage.pageElement).classList.toggle("active");
-        el(this.currentPage.pageElement).style.display = "block";
-
-        // set question bits
-        if (this.currentPage.pageType == PageType.Question) {
-            this.setQuestion();
-        }
-
-        this.setBG();
-
-    }
-
-    private getNextPage() {
-        this.currentPageIdx++;
-        this.currentPage = this.pages[this.currentPageIdx];
-        this.previousPage = this.pages[this.currentPageIdx-1];
-        this.setCurrentPage();
-    }
-
-    private setBG() {
+    private setBG(color : string) {
         // sets background colour based on page
-        el("body").style.backgroundColor = this.currentPage.bgColour;
+        el("body").style.backgroundColor = color;
     }
 
-    private setQuestion() {
-        if (!this.currentPage.question) return;
-        var q = this.currentPage.question;
-        var e = this.currentPage.pageElement;
+    private showLogin() {
+        this.setBG(this.colours.beige);
+        el("#login").style.display = "block";
 
-        // set question copy
-        el(e + " .question p").innerHTML = q.question;
+        // animate
+        TweenMax.from(".theSoundOf path", 0.5, {opacity:0, y:-50, stagger: {each: 0.05, from:"random"}, delay:1});
 
-        switch (q.type) {
-            case QuestionType.Slider:
-                // set slider parameters
-                q = <SliderQuestion>q;
-                var slider = <HTMLInputElement>document.querySelector("#sliderInput");
-                slider.min = q.minValue.toString();
-                slider.max = q.maxValue.toString();
-                break;
+        TweenMax.from(".nectaron path, .nectaron polygon, .nectaron rect", 0.5, {opacity:0, y:50, stagger: {each: 0.02, from:"random"}, delay:1});
 
-            case QuestionType.MultiChoice:
-                break;
+        TweenMax.from("#login .subheading, #login .btn", 1, {opacity:0, delay: 2.1});
 
-            case QuestionType.QuickFire:
-                break;
-        }
+        // TweenMax.to("#login .btn", 0.75, {scale:1.02, repeat:-1, delay:2.85, ease:"linear", yoyo:true, yoyoEase:"linear"})
+    }
 
-        this.currentQuestion = q;
+    private showRoundName() {
+        console.log()
+    }
+
+    private next() {
 
     }
 
-    private sliderChange(e : any) {
-        if (!this.currentPage.question) return;
+    // private setCurrentPage() {
+    //     console.log(this.currentPage);
 
-        // get the width and the value of the slider 
-        this.sliderWidth = e.srcElement.clientWidth;
-        this.sliderValue = e.srcElement.value;
+    //     // hide current page
+    //     if (this.previousPage) {
+    //         // el(this.previousPage.pageElement).classList.toggle("active");
+    //         el(this.previousPage.pageElement).style.display = "none";
+    //     }
 
-        // get the next position of the arrow
-        // move the triangle to match the position of the slider thumb
-        this.sliderArrowEl.style.left = (((this.sliderValue - this.currentQuestion.minValue) / (this.currentQuestion.maxValue - this.currentQuestion.minValue) * (this.sliderWidth)) - this.sliderArrowEl.getBoundingClientRect().width/2).toString() + "px"
+    //     // show the next page
+    //     // el(this.currentPage.pageElement).classList.toggle("active");
+    //     el(this.currentPage.pageElement).style.display = "block";
 
-        this.sliderFruitScaleChange()
-    }
+    //     // set question bits
+    //     if (this.currentPage.pageType == PageType.Question) {
+    //         this.setQuestion();
+    //     }
 
-    private sliderFruitScaleChange() {
-        console.log(this.sliderPreviousValue, this.sliderValue);
+    //     this.setBG();
 
-        if (this.sliderValue < ((this.currentQuestion.maxValue - this.currentQuestion.minValue) / 2) ) {
-            // bottom fruit
-            this.sliderBottomFruitEl.style.width = px(this.sliderBottomFruitEl.getBoundingClientRect().width + (this.sliderPreviousValue < this.sliderValue ? -2 : 2));
-            this.sliderBottomFruitEl.style.top = px(parseInt(getComputedStyle(this.sliderBottomFruitEl).top.replace(/[^\d-]/g, "")) + (this.sliderPreviousValue < this.sliderValue ? 1 : -1))
-        } else {
-            // top fruit
-            this.sliderTopFruitEl.style.width = px(this.sliderTopFruitEl.getBoundingClientRect().width + (this.sliderPreviousValue < this.sliderValue ? 2 : -2))
-            this.sliderTopFruitEl.style.bottom = px(parseInt(getComputedStyle(this.sliderTopFruitEl).bottom.replace(/[^\d-]/g, "")) + (this.sliderPreviousValue < this.sliderValue ? -1 : 1))
-        }
+    // }
 
-        this.sliderPreviousValue = this.sliderValue;  
-    }
+    // private getNextPage() {
+    //     this.currentPageIdx++;
+    //     this.currentPage = this.pages[this.currentPageIdx];
+    //     this.previousPage = this.pages[this.currentPageIdx-1];
+    //     this.setCurrentPage();
+    // }
 
-    private scaleFruit(el: HTMLElement, increment : number) {
-        el.style.width = (el.getBoundingClientRect().width + increment).toString() + "px";
-    }
+
+    // private setQuestion() {
+    //     if (!this.currentPage.question) return;
+    //     var q = this.currentPage.question;
+    //     var e = this.currentPage.pageElement;
+
+    //     // set question copy
+    //     el(e + " .question p").innerHTML = q.question;
+
+    //     switch (q.type) {
+    //         case QuestionType.Slider:
+    //             // set slider parameters
+    //             q = <SliderQuestion>q;
+    //             var slider = <HTMLInputElement>document.querySelector("#sliderInput");
+    //             slider.min = q.minValue.toString();
+    //             slider.max = q.maxValue.toString();
+    //             break;
+
+    //         case QuestionType.MultiChoice:
+    //             break;
+
+    //         case QuestionType.QuickFire:
+    //             break;
+    //     }
+
+    //     this.currentQuestion = q;
+
+    // }
+
+    // private sliderChange(e : any) {
+    //     if (!this.currentPage.question) return;
+
+    //     // get the width and the value of the slider 
+    //     this.sliderWidth = e.srcElement.clientWidth;
+    //     this.sliderValue = e.srcElement.value;
+
+    //     // get the next position of the arrow
+    //     // move the triangle to match the position of the slider thumb
+    //     this.sliderArrowEl.style.left = (((this.sliderValue - this.currentQuestion.minValue) / (this.currentQuestion.maxValue - this.currentQuestion.minValue) * (this.sliderWidth)) - this.sliderArrowEl.getBoundingClientRect().width/2).toString() + "px"
+
+    //     this.sliderFruitScaleChange()
+    // }
+
+    // private sliderFruitScaleChange() {
+    //     console.log(this.sliderPreviousValue, this.sliderValue);
+
+    //     if (this.sliderValue < ((this.currentQuestion.maxValue - this.currentQuestion.minValue) / 2) ) {
+    //         // bottom fruit
+    //         this.sliderBottomFruitEl.style.width = px(this.sliderBottomFruitEl.getBoundingClientRect().width + (this.sliderPreviousValue < this.sliderValue ? -2 : 2));
+    //         this.sliderBottomFruitEl.style.top = px(parseInt(getComputedStyle(this.sliderBottomFruitEl).top.replace(/[^\d-]/g, "")) + (this.sliderPreviousValue < this.sliderValue ? 1 : -1))
+    //     } else {
+    //         // top fruit
+    //         this.sliderTopFruitEl.style.width = px(this.sliderTopFruitEl.getBoundingClientRect().width + (this.sliderPreviousValue < this.sliderValue ? 2 : -2))
+    //         this.sliderTopFruitEl.style.bottom = px(parseInt(getComputedStyle(this.sliderTopFruitEl).bottom.replace(/[^\d-]/g, "")) + (this.sliderPreviousValue < this.sliderValue ? -1 : 1))
+    //     }
+
+    //     this.sliderPreviousValue = this.sliderValue;  
+    // }
+
+    // private scaleFruit(el: HTMLElement, increment : number) {
+    //     el.style.width = (el.getBoundingClientRect().width + increment).toString() + "px";
+    // }
 
     // CALLBACK FROM APP
     public loginSuccessful() {
         console.log("login successful");
-        this.getNextPage();
     }
 
     public OnUserData(type: si.DataType, data: si.Data): void {
@@ -197,7 +290,7 @@ export default class UI {
 
 }
 
-function el(e: string) {
+export function el(e: string) {
     return <HTMLElement>document.querySelector(e);
 }
 
