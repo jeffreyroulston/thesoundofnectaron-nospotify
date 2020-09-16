2fd1e4890d8dc667ba5987659018e457c836e07d
Celine <celinegiovanni92@gmail.com>
2020-09-16 17:03:40 +1200

    function naming lol

--- client/src/ts/app.ts
+++ client/src/ts/app.ts
@@ -11,7 +11,9 @@ import { easeExpIn } from "d3";
 import { shuffle } from "./helpers";
 
 let CLIENT_ID: string = 'c5a5170f00bf40e2a89be3510402947c';
-let REDIRECT_URI: string = "http://localhost:8888";
+let REDIRECT_URI: string = "http://10.100.10.63:8888";
+// let REDIRECT_URI: string = "http://localhost:8888";
+// let REDIRECT_URI: string = "http://thesoundofnectaron.truedigital.co.nz";
 let SCOPES: string[] = [
     'user-top-read', 
     'user-read-private', 
@@ -34,7 +36,7 @@ export default class App {
     private topTracks: si.Track[] | undefined;
     // private answeredQuestions: Questions.Question[] = [];
 
-    private requestedPlaylistLength: number = 60;
+    private requestedPlaylistLength: number = 120;
 
     constructor() {
         // console.log(window.location.href);
@@ -133,7 +135,8 @@ export default class App {
                 let q = data.sliderQuestions[i];
 
                 if (q.params === si.QueryParameters.PlaylistLength) {
-                    this.requestedPlaylistLength = q.answer;
+                    this.requestedPlaylistLength = q.answer
+                    console.log("requested playlist length", this.requestedPlaylistLength)
                 }
 
                 else {
@@ -254,13 +257,13 @@ export default class App {
                     this.spotifyInterface.CreatePlaylist({
                         UserId: this.profile.id,
                         TrackUris: trackUris,
-                        Name: "Nectaron",
-                        Description: "Get a load of this ya jabronies",
+                        Name: "The Sound of Nectaron",
+                        Description: "",
                         Public: false,
                         Image: {
                             Width: 72,
                             Height: 72,
-                            Url: "http://localhost:8888/assets/albumCover.jpg"
+                            Url: "http://thesoundofnectaron.truedigital.co.nz/assets/albumCover.jpg"
                         }
                     });
                 }
